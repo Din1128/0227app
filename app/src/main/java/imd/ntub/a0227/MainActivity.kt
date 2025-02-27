d24d77332696be8a1ba52ef427d12d4aae22903e
Din1128 <emilydin1128@gmail.com>
2025-02-27 17:30:42 +0800

    Add files via upload

---
+++ app/src/main/java/imd/ntub/a0227/MainActivity.kt
@@ -0,0 +1,11 @@
+package imd.ntub.a0227
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
